fa9d8eecedb024a6bbf72f7519a3956273766a3d
Philipp Parzer <phil0898.pp@gmail.com>
2022-10-27 19:37:19 +0200

    add redirect to /  on  navigation to  /singleplayer on desktop device

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,25 @@
 // middleware.ts
 import { NextResponse, userAgent } from "next/server";
 import type { NextRequest } from "next/server";
+import { basename } from "path";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
   const { device } = userAgent(request);
   const viewport = device.type === "mobile" ? "mobile" : "desktop";
-  if (viewport === "mobile") {
+  console.log(url)
+  console.log(viewport)
+  if (viewport === "mobile" && url.pathname !== "/singleplayer") {
     return NextResponse.redirect(new URL("/singleplayer", url));
+  }
+
+  else if (viewport !== "mobile" && url.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", url.origin))
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/singleplayer"]
 };
