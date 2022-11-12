3416a94edc41b028c56eebf02fa65703b34ef717
Philipp Parzer <phil0898.pp@gmail.com>
2022-11-12 16:05:27 +0100

    add favorites sorting

--- components/static/HomeBox.tsx
+++ components/static/HomeBox.tsx
@@ -10,119 +10,172 @@ import { FilterOptions } from "../../shared-ts/enums";
 import FilterSelect from "./FilterSelect";
 import IAM from "./IAM";
 import { motion } from "framer-motion";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 interface Props {
-  projects: any,
-  cookie: any,
-  about: any,
-  IAMStrings: any
+  projects: any;
+  cookie: any;
+  about: any;
+  IAMStrings: any;
 }
 
-let projectCounter:number = 0;
+let projectCounter: number = 0;
+let slicedSortedFavorites: string[];
 
-export default function HomeBox({ projects, cookie, about, IAMStrings }: Props) {
+export default function HomeBox({projects, cookie, about, IAMStrings}: Props) {
   const [filterOption, setFilterOption] = useState<FilterOptions>(
     FilterOptions.Recents
   );
-    
-
-  const [searchEnabled, setSearchEnabled] = useState<boolean>(false);
 
   const updateFilter = (option: number) => {
     setFilterOption(option);
     projectCounter = 0;
   };
 
+  const { isLoading, isError, data } = useQuery("favorites", () =>
+    axios.get("/api/get-favorites")
+  );
+  const favoritesArray = data?.data;
+
+  if (!isLoading && !isError) {
+    const sortedFavorites = [...favoritesArray].sort(
+      (a: any, b: any) => b.faves - a.faves
+    );
+    slicedSortedFavorites = sortedFavorites.slice(0, 4);
+  }
+
   return (
     <>
       <div className="relative w-11/12 lg:w-7/12 sm:max-w-[895px]">
-        <IAM IAMStrings={IAMStrings}/>
+        <IAM IAMStrings={IAMStrings} />
       </div>
       <div className="relative w-11/12 lg:w-7/12 sm:max-w-[895px] h-[70%] lg:h-[65%] lg:max-h-[567.54px] rounded-[30px] card">
         <div className="absolute w-full h-full rounded-[30px] -z-10 card-noise"></div>
-        <div className="absolute
-         top-5 w-full flex justify-end">
+        <div
+          className="absolute
+         top-5 w-full flex justify-end"
+        >
           {/* <CursorCustomizer cookie={cookie} /> TODO: uncomment when liveblocks */}
         </div>
         <div className="flex flex-col lg:flex-row h-full">
           <div className="flex flex-col justify-center border-b-2 lg:border-r-2 lg:border-b-0 border-trans-white lg:w-[20%]">
             <div className="h-[70%] filter-list-desktop text-white">
-              <FilterList updateFilter={updateFilter} filterOption={filterOption}/>
+              <FilterList
+                updateFilter={updateFilter}
+                filterOption={filterOption}
+              />
             </div>
             <div className="flex h-[58px] lg:h-full lg:pt-20 filter-select-desktop w-full px-5 lg:items-start items-center lg:justify-center">
-              <FilterSelect updateFilter={updateFilter} filterOption={filterOption} />
+              <FilterSelect
+                updateFilter={updateFilter}
+                filterOption={filterOption}
+              />
             </div>
           </div>
 
           <div className="flex flex-col w-full px-5 lg:px-10 py-10 lg:py-20 text-white lg:w-[80%] overflow-auto">
-          
             <ul className="flex flex-row flex-wrap gap-7 lg:gap-10">
-              {
-                
-                  projects.map((link: any) => {
-                    let linkTags = link.data.category.toLowerCase()
+              {projects.map((link: any) => {
+                let linkTags = link.data.category.toLowerCase();
+                let linkIsRecent = link.data.isRecent;
 
-                    if (filterOption === FilterOptions.Recents) {
+                if (!isLoading && !isError) {
+                  let slicedSortedFavoriteLookups = slicedSortedFavorites.map(
+                    (item: any) => item.project
+                  );
+
+                  if (filterOption === FilterOptions.Favorites) {
+                    if (
+                      slicedSortedFavoriteLookups.includes(
+                        link.data.databaseLookup
+                      )
+                    ) {
                       projectCounter++;
-                      if (projectCounter > 0) {
-                        return (
-                          <motion.li
-                            layout
-                            key={link.name}
-                            animate={{ scale: 1, opacity: 1 }}
-                            initial= {{ scale: .8, opacity: .8}}
-                            transition={{ type: "spring", bounce: 0.25, delay: 0}}
-                          >
-                            <ProjectLink
-                              url={link.data.link.value.data.url}
-                              color={link.data.color}
-                              name={link.name}
-                              altText={link.data.logoAltText}
-                              logoPath={link.data.logo}
-                            />
-                          </motion.li>
-                          
-                          
-                        );
-                      }
-                    }
 
-                    else if (linkTags.includes(FilterOptions[filterOption].toLowerCase())) {
-                      projectCounter++;
-                      if (projectCounter > 0) {
-                        return (
-                          
-                          <motion.li
-                            layout
-                            key={link.name}
-                            animate={{ scale: 1, opacity: 1 }}
-                            initial= {{ scale: .8, opacity: .8}}
-                            transition={{ type: "spring", bounce: 0.25, delay: 0}}
-                          >
-                            <ProjectLink
-                              url={link.data.link.value.data.url}
-                              color={link.data.color}
-                              name={link.name}
-                              altText={link.data.logoAltText}
-                              logoPath={link.data.logo}
-                            />
-                          </motion.li>
-                          
-                          
-                        );
-                      }
+                      return (
+                        <motion.li
+                          layout
+                          key={link.name}
+                          animate={{ scale: 1, opacity: 1 }}
+                          initial={{ scale: 0.8, opacity: 0.8 }}
+                          transition={{
+                            type: "spring",
+                            bounce: 0.25,
+                            delay: 0,
+                          }}
+                        >
+                          <ProjectLink
+                            url={link.data.link.value.data.url}
+                            color={link.data.color}
+                            name={link.name}
+                            altText={link.data.logoAltText}
+                            logoPath={link.data.logo}
+                          />
+                        </motion.li>
+                      );
                     }
+                  }
+                }
 
-                  })
+                if (filterOption === FilterOptions.Recents) {
+                  if (linkIsRecent) {
+                    projectCounter++;
+                    return (
+                      <motion.li
+                        layout
+                        key={link.name}
+                        animate={{ scale: 1, opacity: 1 }}
+                        initial={{ scale: 0.8, opacity: 0.8 }}
+                        transition={{ type: "spring", bounce: 0.25, delay: 0 }}
+                      >
+                        <ProjectLink
+                          url={link.data.link.value.data.url}
+                          color={link.data.color}
+                          name={link.name}
+                          altText={link.data.logoAltText}
+                          logoPath={link.data.logo}
+                        />
+                      </motion.li>
+                    );
+                  }
+                } else if (
+                  linkTags.includes(FilterOptions[filterOption].toLowerCase())
+                ) {
+                  projectCounter++;
+                  if (projectCounter > 0) {
+                    return (
+                      <motion.li
+                        layout
+                        key={link.name}
+                        animate={{ scale: 1, opacity: 1 }}
+                        initial={{ scale: 0.8, opacity: 0.8 }}
+                        transition={{ type: "spring", bounce: 0.25, delay: 0 }}
+                      >
+                        <ProjectLink
+                          url={link.data.link.value.data.url}
+                          color={link.data.color}
+                          name={link.name}
+                          altText={link.data.logoAltText}
+                          logoPath={link.data.logo}
+                        />
+                      </motion.li>
+                    );
+                  }
                 }
-                
-                {/*Render something if no projects for current selection*/}
-                {projectCounter === 0 && (
-                  filterOption === FilterOptions.About 
-                  ? <>{about}</>
-                  : <>nothing to see here</> )
-                  
-                }
+              })}
+
+              {/*Render something if no projects for current selection*/}
+              {projectCounter === 0 &&
+                (filterOption === FilterOptions.About ? (
+                  <>{about}</>
+                ) : isLoading ? (
+                  <>loading...</>
+                ) : isError ? (
+                  <>hmm something went wrong</>
+                ) : (
+                  <>nothing to see here</>
+                ))}
             </ul>
           </div>
         </div>
---
+++ pages/api/get-favorites.tsx
@@ -0,0 +1,23 @@
+import { connect } from "./db";
+
+export default async function handler(req: any, res: any) {
+  let data = req.body;
+ 
+  if (req.method === "GET") {
+    console.log(data)
+    const mongoDB = await connect("projects");
+
+    const entry = await mongoDB?.collection.find().toArray();
+    console.log(entry)
+
+    if (entry) {  
+        mongoDB?.client.close();
+        return res.status(201).json(entry); 
+    }
+
+    mongoDB?.client.close();
+    res.status(500).json({ message: "ERROR" });
+    return;
+  }
+
+}
