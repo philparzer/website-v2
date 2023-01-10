4ada6b795fc63ae9a94db7b9ffe13f3a14a72f9b
philparzer <phil0898.pp@gmail.com>
2023-01-10 14:24:24 +0100

    change favicon path

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,10 +72,10 @@ export default function Page(props: any) {
           name="description"
           content={localizedStaticContent.metaHome[props.locale]}
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
-          <link rel="manifest" href="favicon/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#ef476a" />
           <meta name="theme-color" content="#ffe7d8" />
       </Head>
