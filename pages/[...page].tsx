eac16ab04bd43c8c5c998386d5de9ee23f945c3d
philparzer <phil0898.pp@gmail.com>
2023-01-10 14:23:16 +0100

    change favicon

--- pages/[...page].tsx
+++ pages/[...page].tsx
@@ -117,10 +117,10 @@ export default function Page({ page, link, links, locale }: any) {
           content={`/api/og-project?locale=${locale}&lookup=${link.data.databaseLookup}&name=${link.data.link.value.name}&status=${link.data.status}`}
         />
         <meta name="description" content={page?.data.descripton} />
-        <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png" />
-        <link rel="manifest" href="../favicon/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${locale !== "en" && "../"}favicon/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${locale !== "en" && "../"}favicon/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${locale !== "en" && "../"}favicon/favicon-16x16.png`} />
+        <link rel="manifest" href={`${locale !== "en" && "../"}favicon/site.webmanifest`} />
         <meta name="msapplication-TileColor" content="#ef476a" />
         <meta name="theme-color" content="#ffe7d8" />
       </Head>
